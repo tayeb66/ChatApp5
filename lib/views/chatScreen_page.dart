1cc60e94929eddf53504b9ac42f35ebe28a967bc
tayeb ahmed <tayebahmed@gmail.com>
2022-03-21 16:00:06 +0600

    design chatScreen page

--- lib/views/chatScreen_page.dart
+++ lib/views/chatScreen_page.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -9,6 +10,10 @@ class ChatScreenPage5 extends StatefulWidget {
 }
 
 class _ChatScreenPage5State extends State<ChatScreenPage5> {
+
+  var message = TextEditingController();
+  var storeMessage = FirebaseFirestore.instance;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,11 +21,53 @@ class _ChatScreenPage5State extends State<ChatScreenPage5> {
         title: Text('ChatScreenPage5'),
         centerTitle: true,
         actions: [
-          IconButton(onPressed: () async{
-            SharedPreferences preferences = await SharedPreferences.getInstance();
-            Navigator.pop(context);
-            preferences.clear();
-          }, icon: Icon(Icons.logout)),
+          IconButton(
+              onPressed: () async {
+                SharedPreferences preferences =
+                    await SharedPreferences.getInstance();
+                Navigator.pop(context);
+                preferences.clear();
+              },
+              icon: Icon(Icons.logout)),
+        ],
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+            alignment: Alignment.center,
+            child: Text(
+              'Message',
+              textScaleFactor: 1.3,
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 12.0),
+                  child: Container(
+                    margin: EdgeInsets.only(bottom: 20.0),
+                    child: TextFormField(
+                      controller: message,
+                      decoration: InputDecoration(
+                        hintText: 'Write message',
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              IconButton(
+                  onPressed: () {
+                    message.clear();
+                  },
+                  icon: Icon(
+                    Icons.send,
+                    color: Colors.blue,
+                  )),
+            ],
+          ),
         ],
       ),
     );
